f2fdeb14d904bdcce288eb0f2485ab4971974807
iamritvik <khanijo20501972@gmail.com>
2020-03-17 23:02:25 +0530

    menu and signup added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
